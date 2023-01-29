72bab09d9a6ae52a0b7e18c0ccec663c8fcb2efb
ivann <ivan.ng0515@gmail.com>
2023-01-29 12:13:04 +0800

    commit

--- rummikub/deck.go
+++ rummikub/deck.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
-	""
+	"math/rand"
+	"time"
 )
 
 type full_deck []string
 
 func newDeck() full_deck {
+	// deck will automatically be randomized on newDeck()
 	full_deck := full_deck{}
 	cardNum := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"}
 	cardColor := []string{"R", "Y", "B", "BL"}
@@ -22,6 +24,13 @@ func newDeck() full_deck {
 	}
 	full_deck = append(full_deck, "Joker1")
 	full_deck = append(full_deck, "Joker2")
+	// randomization
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for i := range full_deck {
+		newPos := r.Intn(len(full_deck) - 1)
+		full_deck[i], full_deck[newPos] = full_deck[newPos], full_deck[i]
+	}
 
 	return full_deck
 }
@@ -30,8 +39,4 @@ func (d full_deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
-}
-
-func savetofile() {
-
 }
