5ed6effcdd1d423c933548f328e5241629898cfd
ivann <ivan.ng0515@gmail.com>
2023-01-30 15:10:01 +0800

    commit

--- primenum/main.go
+++ primenum/main.go
@@ -12,15 +12,15 @@ func main() {
 	pos_fac := 1
     factor_list := []int{}
 
-	fmt.Print("Insert a number: ")
-	fmt.Scan(&usr_in)
-	usr_num, err := strconv.Atoi(usr_in)
-	if err != nil {
-		os.Exit(1)
-	}
-	for usr_num-pos_fac != -1 {
+    fmt.Print("Insert a number: ")
+    fmt.Scan(&usr_in)
+    usr_num, err := strconv.Atoi(usr_in)
+    if err != nil {
+        os.Exit(1)
+    }
+    for usr_num-pos_fac != -1 {
         if usr_num%pos_fac == 0 {
-			factors += 1
+            factors += 1
             factor_list = append(factor_list, pos_fac)
         }  
         if factors == 2 && factor_list[1] == usr_num && factor_list[0] == 1 {
