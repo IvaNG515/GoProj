91130f9bf8205d6b22e07e863d4d35094c47de7d
ivann <ivan.ng0515@gmail.com>
2023-01-30 13:42:48 +0800

    commit

--- primenum/main.go
+++ primenum/main.go
@@ -7,9 +7,10 @@ import (
 )
 
 func main() {
-	var usr_in string
-	factors := 0
+    var usr_in string
+    factors := 0
 	pos_fac := 1
+    factor_list := []int{}
 
 	fmt.Print("Insert a number: ")
 	fmt.Scan(&usr_in)
@@ -18,16 +19,16 @@ func main() {
 		os.Exit(1)
 	}
 	for usr_num-pos_fac != -1 {
-		if usr_num%pos_fac == 0 {
+        if usr_num%pos_fac == 0 {
 			factors += 1
-			pos_fac += 1
-		} else {
-			pos_fac += 1
-		}
-	}
-	if factors == 2 {
-		fmt.Println("Is prime")
-	} else {
-		fmt.Println("Not Prime")
-	}
+            factor_list = append(factor_list, pos_fac)
+        }  
+        if factors == 2 && factor_list[1] == usr_num && factor_list[0] == 1 {
+            fmt.Println("Is prime")
+            break
+        } else if factors > 2 {
+            fmt.Println("Not prime")
+        }
+        pos_fac += 1
+    }
 }
